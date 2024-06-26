44c2c86d7d4e0b8eb20c45c85c93071e1589677d
Shaxzoda Najmiddinova <najmiddinovashahzoda2005@gmail.com>
2024-06-26 11:53:33 +0500

    privacy policy

--- src/modules/KnowledgeBase/components/PrivacyPolicy.vue
+++ src/modules/KnowledgeBase/components/PrivacyPolicy.vue
@@ -3,22 +3,47 @@
 //IMPORTED FILES
 import Tabs from "@/components/Tab/Tabs.vue";
 import Tab from "@/components/Tab/Tab.vue";
-import {onMounted, ref, watch} from "vue";
-import knowledgeBase from ".././store/index"
+import {onMounted, ref, watch, Ref, computed} from "vue";
 import {toast} from "vue3-toastify";
 import {useI18n} from "vue-i18n";
+import knowledgeBase from ".././store/index"
+import useVuelidate, {Validation} from "@vuelidate/core";
+import {required, helpers} from '@vuelidate/validators'
 
 //DECLARED VARIABLES
 const store = knowledgeBase()
-const is_disabledUz = ref(false)
-const is_disabledKr = ref(false)
-const is_disabledRu = ref(false)
-const {t} = useI18n()
 const props = defineProps<{
   knowledge: string
 }>();
+const isEdit = ref(false)
+const content = ref({
+  text: "",
+  text_uz: "",
+  text_ru: "",
+  text_kr: ""
+})
+const {t} = useI18n()
 
-//MOUNTED LIFE CYCLE
+
+// validations
+const rules = computed(() => {
+  return {
+    text_uz: {
+      required: helpers.withMessage(t('required_password'), required)
+    },
+    text_ru: {
+      required: helpers.withMessage(t('required_password'), required)
+    },
+    text_kr: {
+      required: helpers.withMessage(t('required_password'), required)
+    },
+  };
+});
+
+const validate: Ref<Validation> = useVuelidate(rules, content)
+
+  
+//MOUNTED
 onMounted(async () => {
   let knowledgeBase = localStorage.getItem('knowledgeBase')
   if (knowledgeBase == 'PrivacyPolicy') {
@@ -34,116 +59,161 @@ watch(() => props.knowledge, async function (val) {
   }
 })
 
+
 //FUNCTIONS
 const refresh = async () => {
   try {
     await store.getPagesPolicy({type: 'privacy_policy'});
+
+    if (Object.keys(store.pagesListPolicy).length) {
+      content.value = store.pagesListPolicy
+    } else {
+      content.value = {
+        text: "",
+        text_uz: "",
+        text_ru: "",
+        text_kr: ""
+      }
+    }
   } catch (error: any) {
     toast.error(t('error'));
   }
 };
 
 
+const createText = async () => {
+  const result = await validate.value.$validate()
+
+  if (result) {
+    try {
+      await store.createPages(content.value).then(() => {
+        toast.success(t('created_successfully'));
+        refresh()
+      })
+
+    } catch (err: any) {
+      toast.error(err.message)
+    }
+  } else {
+    toast.error(t('validation.all_fields_are_required'))
+  }
+}
+
 const updateText = async () => {
-  store.pagesListPolicy.text = store.pagesListPolicy.text_uz
-  await store.updatePages(store.pagesListPolicy)
-  await refresh()
-  toast.success(t("updated_successfully"));
-  is_disabledUz.value = false
+  const result = await validate.value.$validate()
 
-}
-const updateText2 = async () => {
-  store.pagesListPolicy.text = store.pagesListPolicy.text_uz
-  await store.updatePages(store.pagesListPolicy)
-  await refresh()
-  toast.success(t("updated_successfully"));
-  is_disabledRu.value = false
-}
-const updateText3 = async () => {
-  store.pagesListPolicy.text = store.pagesListPolicy.text_uz
-  await store.updatePages(store.pagesListPolicy)
-  await refresh()
-  toast.success(t("updated_successfully"));
-  is_disabledKr.value = false
-}
+  if (result) {
+    try {
+      content.value.text = content.value.text_uz
+      if (store.pagesListPolicy.id) {
+        await store.updatePages({id: store.pagesListPolicy.id, data: content.value}).then(() => {
+          toast.success(t('success'));
+          refresh()
+        })
+      } else {
+        await store.createPages(content.value)
+      }
+
+      isEdit.value = false
 
 
+    } catch (err: any) {
+      toast.error(err.message)
+    }
+  } else {
+    toast.error(t('validation.all_fields_are_required'))
+  }
+}
+
+const deletePrivacyPolicy = async () => {
+  isEdit.value = false
+}
 </script>
 
 <template>
+
+
   <div class="card">
-    <Tabs pill>
-      <Tab title="UZ">
-        <div v-if="store.pagesListPolicy.text_uz">
-          <Editor
-              :placeholder="$t('enter_information')"
-              content-type="html"
-              toolbar="full"
-              class="scrollbar rounded border"
-              style="height: 45vh; overflow-y: auto;"
-              v-model:content="store.pagesListPolicy.text_uz"
-          >
-          </Editor>
-        </div>
+    <div>
+      <Tabs pill>
+        <Tab title="UZ">
+          <div v-if="!store.pagesListPolicy.id || isEdit">
+            <Editor :placeholder="t('enter_information')" content-type="html" toolbar="full"
+                         class="scrollbar rounded border" style="height: 45vh; overflow-y: auto;"
+                         v-model:content="content.text_uz">
+            </Editor>
+            <p v-for="error in validate.text_uz.$errors" :key="error.$uid" class="text-danger text-sm">
+              {{ t(error.$message) }}
+            </p>
+          </div>
 
-        <div class="flex justify-end mt-2">
-          <button v-if="is_disabledUz == false" class="btn-warning" @click="is_disabledUz = true">{{
-              t('Edit')
+          <div v-else :uk-tooltip="$t('Edit')" class="cursor-pointer" @click="isEdit = true">
+            <div class="border-4 p-4 rounded-lg my-4">
+              <div v-html="store.pagesListPolicy.text_uz"></div>
+            </div>
+          </div>
+        </Tab>
+
+
+        <Tab title="KR">
+          <div v-if="!store.pagesListPolicy.id || isEdit">
+            <Editor :placeholder="t('enter_information')" content-type="html" toolbar="full"
+                         class="scrollbar rounded border" style="height: 45vh; overflow-y: auto;"
+                         v-model:content="content.text_kr">
+            </Editor>
+
+            <p v-for="error in validate.text_kr.$errors" :key="error.$uid" class="text-danger text-sm">
+              {{ t(error.$message) }}
+            </p>
+          </div>
+
+          <div v-else :uk-tooltip="$t('Edit')" class="cursor-pointer" @click="isEdit = true">
+            <div class="border-4 p-4 rounded-lg my-4">
+              <div v-html="store.pagesListPolicy.text_kr"></div>
+            </div>
+          </div>
+        </Tab>
+
+
+        <Tab title="RU">
+          <div v-if="!store.pagesListPolicy.id || isEdit">
+            <Editor :placeholder="t('enter_information')" content-type="html" toolbar="full"
+                         class="scrollbar rounded border" style="height: 45vh; overflow-y: auto;"
+                         v-model:content="content.text_ru">
+            </Editor>
+
+            <p v-for="error in validate.text_ru.$errors" :key="error.$uid" class="text-danger text-sm">
+              {{ t(error.$message) }}
+            </p>
+          </div>
+
+          <div v-else :uk-tooltip="$t('Edit')" class="cursor-pointer" @click="isEdit = true">
+            <div class="border-4 p-4 rounded-lg my-4">
+              <div v-html="store.pagesListPolicy.text_ru"></div>
+            </div>
+          </div>
+        </Tab>
+      </Tabs>
+
+
+      <div class="flex justify-end">
+        <div class="flex items-center justify-end mt-4 gap-1" v-if="store.pagesListPolicy?.id && isEdit">
+          <button class="btn-secondary mr-3" @click="deletePrivacyPolicy">{{
+              t('Cancel')
             }}
           </button>
 
-          <button v-if="is_disabledUz == true" class="btn-success" @click="updateText">{{ t('Save') }}</button>
-
-        </div>
-      </Tab>
-      <Tab title="KR">
-        <div v-if="store.pagesListPolicy.text_kr">
-          <Editor
-
-              :placeholder="$t('enter_information')"
-              content-type="html"
-              toolbar="full"
-              class="scrollbar rounded border"
-              style="height: 45vh; overflow-y: auto;" v-model:content="store.pagesListPolicy.text_kr"
-          >
-
-          </Editor>
-        </div>
-
-        <div class="flex justify-end mt-2">
-          <button v-if="is_disabledKr == false" class="btn-warning" @click="is_disabledKr = true">{{
+          <button class="btn-warning" @click="updateText">{{
               t('Edit')
             }}
           </button>
-
-          <button v-if="is_disabledKr == true" class="btn-success" @click="updateText3">{{ t('Save') }}</button>
-
-        </div>
-      </Tab>
-
-
-      <Tab title="RU">
-        <div v-if="store.pagesListPolicy.text_ru">
-          <Editor
-              :placeholder="$t('enter_information')"
-              content-type="html"
-              toolbar="full"
-              class="scrollbar rounded border"
-              style="height: 45vh; overflow-y: auto;" v-model:content="store.pagesListPolicy.text_ru"
-          >
-
-          </Editor>
         </div>
 
-        <div class="flex justify-end mt-2">
-          <button v-if="is_disabledRu == false" class="btn-warning" @click="is_disabledRu = true">{{
-              t('Edit')
-            }}
-          </button>
-
-          <button v-if="is_disabledRu == true" class="btn-success" @click="updateText2">{{ t('Save') }}</button>
-        </div>
-      </Tab>
-    </Tabs>
+        <button v-if="!store.pagesListPolicy?.id && !isEdit" class="btn-success mt-4" @click="createText">{{
+            t('Save')
+          }}
+        </button>
+      </div>
+    </div>
   </div>
 </template>
