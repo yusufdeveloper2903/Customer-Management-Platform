a31714b57864280d9fe6f6446c844993ef27eb76
Shaxzoda Najmiddinova <najmiddinovashahzoda2005@gmail.com>
2024-07-16 12:28:57 +0500

    bug fix

--- src/modules/KnowledgeBase/components/NearbyPlaces/components/AddEditNearbyPlaces.vue
+++ src/modules/KnowledgeBase/components/NearbyPlaces/components/AddEditNearbyPlaces.vue
@@ -106,6 +106,7 @@ const onSubmit = async () => {
 const hideModal = () => {
   openTime.value = '';
   closeTime.value = '';
+  search.value = ''
   general.tabs = ''
   emptyData.value = false
   Object.assign(location, {
